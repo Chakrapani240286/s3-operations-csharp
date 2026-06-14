fbbb3054a68323d5bf0920469eeb5b401ea7fc94
Chakrapani Upadhyaya <chakrapani240286@gmail.com>
2026-06-14 15:01:32 +0530

    Initial

---
+++ S3BucketExample/Program.cs
@@ -0,0 +1,174 @@
+﻿using S3BucketExample;
+
+Console.Title = "S3 Bucket Example";
+
+while (true)
+{
+    Console.Clear();
+
+    Console.WriteLine("=================================");
+    Console.WriteLine("          S3 OPERATIONS");
+    Console.WriteLine("=================================");
+    Console.WriteLine("1.  Create Bucket");
+    Console.WriteLine("2.  List Buckets");
+    Console.WriteLine("3.  Delete Bucket");
+    Console.WriteLine("4.  Upload File");
+    Console.WriteLine("5.  List Files");
+    Console.WriteLine("6.  Download File");
+    Console.WriteLine("7.  Delete Object");
+    Console.WriteLine("8.  Generate Presigned URL");
+    Console.WriteLine("9.  Enable Versioning");
+    Console.WriteLine("10. Get Versioning Status");
+    Console.WriteLine("11. List Object Versions");
+    Console.WriteLine("12. Download Specific Version");
+    Console.WriteLine("13. Delete Specific Version");
+    Console.WriteLine("0.  Exit");
+    Console.WriteLine("=================================");
+
+    Console.Write("Choice : ");
+
+    if (!int.TryParse(Console.ReadLine(), out int option))
+        continue;
+
+    Console.WriteLine();
+
+    switch (option)
+    {
+        case 1:
+            Console.Write("Bucket Name : ");
+            await S3Methods.CreateBucket(Console.ReadLine()!);
+            break;
+
+        case 2:
+            await S3Methods.ListBuckets();
+            break;
+
+        case 3:
+            Console.Write("Bucket Name : ");
+            await S3Methods.DeleteBucket(Console.ReadLine()!);
+            break;
+
+        case 4:
+            Console.Write("Bucket Name : ");
+            string bucket = Console.ReadLine()!;
+
+            Console.Write("File Path : ");
+            string path = Console.ReadLine()!;
+
+            Console.Write("Object Key : ");
+            string key = Console.ReadLine()!;
+
+            await S3Methods.UploadFileAsync(bucket, key, path);
+            break;
+
+        case 5:
+            Console.Write("Bucket Name : ");
+            await S3Methods.ListFileAsync(Console.ReadLine()!);
+            break;
+
+        case 6:
+            Console.Write("Bucket Name : ");
+            string bucketDownload = Console.ReadLine()!;
+
+            Console.Write("Object Key : ");
+            string keyDownload = Console.ReadLine()!;
+
+            Console.Write("Download Folder : ");
+            string folder = Console.ReadLine()!;
+
+            await S3Methods.DownloadFileAsync(
+                bucketDownload,
+                keyDownload,
+                folder);
+            break;
+
+        case 7:
+            Console.Write("Bucket Name : ");
+            string bucketDelete = Console.ReadLine()!;
+
+            Console.Write("Object Key : ");
+            string keyDelete = Console.ReadLine()!;
+
+            await S3Methods.DeleteObjectAsync(
+                bucketDelete,
+                keyDelete);
+            break;
+
+        case 8:
+            Console.Write("Bucket Name : ");
+            string bucketUrl = Console.ReadLine()!;
+
+            Console.Write("Object Key : ");
+            string keyUrl = Console.ReadLine()!;
+
+            S3Methods.GeneratePreSignedUrl(
+                bucketUrl,
+                keyUrl);
+            break;
+
+        case 9:
+            Console.Write("Bucket Name : ");
+            await S3Methods.EnableVersioning(
+                Console.ReadLine()!);
+            break;
+
+        case 10:
+            Console.Write("Bucket Name : ");
+            await S3Methods.GetVersioningStatus(
+                Console.ReadLine()!);
+            break;
+
+        case 11:
+            Console.Write("Bucket Name : ");
+            await S3Methods.ListObjectVersions(
+                Console.ReadLine()!);
+            break;
+
+        case 12:
+            Console.Write("Bucket Name : ");
+            string bucketVersionDownload = Console.ReadLine()!;
+
+            Console.Write("Object Key : ");
+            string keyVersionDownload = Console.ReadLine()!;
+
+            Console.Write("Version Id : ");
+            string versionIdDownload = Console.ReadLine()!;
+
+            Console.Write("Download Folder : ");
+            string downloadFolder = Console.ReadLine()!;
+
+            await S3Methods.DownloadSpecificVersionAsync(
+                bucketVersionDownload,
+                keyVersionDownload,
+                versionIdDownload,
+                downloadFolder);
+            break;
+
+        case 13:
+            Console.Write("Bucket Name : ");
+            string bucketVersionDelete = Console.ReadLine()!;
+
+            Console.Write("Object Key : ");
+            string keyVersionDelete = Console.ReadLine()!;
+
+            Console.Write("Version Id : ");
+            string versionIdDelete = Console.ReadLine()!;
+
+            await S3Methods.DeleteSpecificVersionAsync(
+                bucketVersionDelete,
+                keyVersionDelete,
+                versionIdDelete);
+            break;
+
+        case 0:
+            return;
+
+        default:
+            Console.WriteLine("Invalid choice.");
+            break;
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Press ENTER to continue...");
+    Console.ReadLine();
+}
---
+++ S3BucketExample/S3Methods.cs
@@ -0,0 +1,219 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace S3BucketExample
+{
+    public static class S3Methods
+    {
+        private static readonly AmazonS3Client _client = new AmazonS3Client();
+
+        public static async Task CreateBucket(string bucketName)
+        {
+            Console.WriteLine("Creating bucket...");
+
+            await _client.PutBucketAsync(new PutBucketRequest
+            {
+                BucketName = bucketName
+            });
+
+            Console.WriteLine("Bucket created successfully.");
+        }
+
+        public static async Task ListBuckets()
+        {
+            Console.WriteLine("\nBuckets:");
+            var response = await _client.ListBucketsAsync();
+
+            foreach (var bucket in response.Buckets)
+            {
+                Console.WriteLine(bucket.BucketName);
+            }
+        }
+
+        public static async Task DeleteBucket(string bucketName)
+        {
+            await _client.DeleteBucketAsync(new DeleteBucketRequest
+            {
+                BucketName = bucketName
+            });
+
+            Console.WriteLine("Bucket deleted.");
+        }
+
+        public static async Task UploadFileAsync(
+            string bucketName,
+            string key,
+            string filePath)
+        {
+            await _client.PutObjectAsync(new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = key,
+                FilePath = filePath
+            });
+
+            Console.WriteLine($"Uploaded: {key}");
+        }
+
+        public static async Task ListFileAsync(string bucketName)
+        {
+            var response = await _client.ListObjectsAsync(new ListObjectsRequest
+            {
+                BucketName = bucketName
+            });
+
+            Console.WriteLine();
+
+            foreach (var item in response.S3Objects)
+            {
+                Console.WriteLine($"{item.Key} ({item.Size} bytes)");
+            }
+        }
+
+        public static async Task DownloadFileAsync(
+            string bucketName,
+            string key,
+            string downloadFolder)
+        {
+            using var response = await _client.GetObjectAsync(
+                bucketName,
+                key);
+
+            Directory.CreateDirectory(downloadFolder);
+
+            string filePath = Path.Combine(downloadFolder, key);
+
+            await response.WriteResponseStreamToFileAsync(
+                filePath,
+                false,
+                CancellationToken.None);
+
+            Console.WriteLine($"Downloaded to {filePath}");
+        }
+
+        public static async Task DeleteObjectAsync(
+            string bucketName,
+            string key)
+        {
+            await _client.DeleteObjectAsync(new DeleteObjectRequest
+            {
+                BucketName = bucketName,
+                Key = key
+            });
+
+            Console.WriteLine("Object deleted.");
+        }
+
+        public static void GeneratePreSignedUrl(
+            string bucketName,
+            string key)
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = bucketName,
+                Key = key,
+                Expires = DateTime.UtcNow.AddMinutes(30)
+            };
+
+            string url = _client.GetPreSignedURL(request);
+
+            Console.WriteLine();
+            Console.WriteLine("PreSigned URL");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(url);
+        }
+
+        public static async Task EnableVersioning(string bucketName)
+        {
+            await _client.PutBucketVersioningAsync(
+                new PutBucketVersioningRequest
+                {
+                    BucketName = bucketName,
+                    VersioningConfig = new S3BucketVersioningConfig
+                    {
+                        Status = VersionStatus.Enabled
+                    }
+                });
+
+            Console.WriteLine("Versioning enabled.");
+        }
+
+        public static async Task GetVersioningStatus(string bucketName)
+        {
+            var response = await _client.GetBucketVersioningAsync(
+                new GetBucketVersioningRequest
+                {
+                    BucketName = bucketName
+                });
+
+            Console.WriteLine();
+            Console.WriteLine($"Versioning Status : {response.VersioningConfig.Status}");
+        }
+
+        public static async Task ListObjectVersions(string bucketName)
+        {
+            var response = await _client.ListVersionsAsync(
+                new ListVersionsRequest
+                {
+                    BucketName = bucketName
+                });
+
+            Console.WriteLine();
+            Console.WriteLine("Versions");
+            Console.WriteLine("-------------------------");
+
+            foreach (var version in response.Versions)
+            {
+                Console.WriteLine(
+                    $"Key : {version.Key}");
+                Console.WriteLine(
+                    $"Version Id : {version.VersionId}");
+                Console.WriteLine(
+                    $"Latest : {version.IsLatest}");
+                Console.WriteLine();
+            }
+        }
+
+        public static async Task DownloadSpecificVersionAsync(
+            string bucketName,
+            string key,
+            string versionId,
+            string downloadFolder)
+        {
+            using var response = await _client.GetObjectAsync(
+                new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = key,
+                    VersionId = versionId
+                });
+
+            Directory.CreateDirectory(downloadFolder);
+
+            string filePath = Path.Combine(downloadFolder, key);
+
+            await response.WriteResponseStreamToFileAsync(
+                filePath,
+                false,
+                CancellationToken.None);
+
+            Console.WriteLine($"Downloaded version {versionId}");
+        }
+
+        public static async Task DeleteSpecificVersionAsync(
+            string bucketName,
+            string key,
+            string versionId)
+        {
+            await _client.DeleteObjectAsync(
+                new DeleteObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = key,
+                    VersionId = versionId
+                });
+
+            Console.WriteLine("Specific version deleted.");
+        }
+    }
+}
